3ac7c2c6b42a30a1a120fc7b10d44d46da8961a3
Sagar Dhadke <peaceunique2003@gmail.com>
2023-05-29 21:00:36 +0530

    Added New Fragments like Home.kt, Service.kt, Wallet.kt, Booking.kt

--- app/src/main/java/net/uniquecomputer/houseguru/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/houseguru/MainActivity.kt
@@ -3,6 +3,7 @@ package net.uniquecomputer.houseguru
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -14,30 +15,40 @@ class MainActivity : AppCompatActivity() {
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.home -> {
-                    // set default activity
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    true
+                   true
                 }
                 R.id.service -> {
-                    // Respond to navigation item 2 click
                     val intent = Intent(this, Services::class.java)
                     startActivity(intent)
+                    overridePendingTransition(0, 0)
                     true
                 }
                 R.id.wallet -> {
                     val intent = Intent(this, Wallet::class.java)
                     startActivity(intent)
+                    overridePendingTransition(0, 0)
                     true
                 }
                 R.id.booking -> {
                    val intent = Intent(this, Booking::class.java)
                         startActivity(intent)
+                        overridePendingTransition(0, 0)
                         true
                 }
                 else -> false
             }
         }
 
+
+
     }
+//
+//    private fun replaceFragment() {
+//        val fragmentManager = supportFragmentManager
+//        val transaction = fragmentManager.beginTransaction()
+//        transaction.replace(
+//            androidx.fragment.R.id.fragment_container_view_tag, Wallet()
+//        transaction.commit()
+//    }
+
 }
--- app/src/main/java/net/uniquecomputer/houseguru/Service.kt
+++ app/src/main/java/net/uniquecomputer/houseguru/Service.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Booking.newInstance] factory method to
+ * Use the [Service.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Booking : Fragment() {
+class Service : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Booking : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_booking, container, false)
+        return inflater.inflate(R.layout.fragment_service, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Booking : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Booking.
+         * @return A new instance of fragment Service.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Booking().apply {
+            Service().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
