e2aff60086983efa64ad4d5b01679c12cdb0f5df
Sagar Dhadke <peaceunique2003@gmail.com>
2023-05-29 21:25:26 +0530

    Fixed All Fragment Bugs

--- app/src/main/java/net/uniquecomputer/houseguru/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/houseguru/MainActivity.kt
@@ -3,52 +3,58 @@ package net.uniquecomputer.houseguru
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import net.uniquecomputer.houseguru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
+        binding.bottomNavigation.setOnItemSelectedListener {
 
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
+            when(it.itemId){
                 R.id.home -> {
-                   true
+                    replaceFragment(Home())
                 }
-                R.id.service -> {
-                    val intent = Intent(this, Services::class.java)
-                    startActivity(intent)
-                    overridePendingTransition(0, 0)
-                    true
+                R.id.Service -> {
+                    replaceFragment(Service())
                 }
-                R.id.wallet -> {
-                    val intent = Intent(this, Wallet::class.java)
-                    startActivity(intent)
-                    overridePendingTransition(0, 0)
-                    true
+                R.id.Wallet -> {
+                    replaceFragment(Wallet())
                 }
-                R.id.booking -> {
-                   val intent = Intent(this, Booking::class.java)
-                        startActivity(intent)
-                        overridePendingTransition(0, 0)
-                        true
+                R.id.Booking -> {
+                    replaceFragment(Booking())
                 }
-                else -> false
+
+                else -> {
+
+                }
+
+
             }
+            true
+
+
         }
 
 
 
     }
-//
-//    private fun replaceFragment() {
-//        val fragmentManager = supportFragmentManager
-//        val transaction = fragmentManager.beginTransaction()
-//        transaction.replace(
-//            androidx.fragment.R.id.fragment_container_view_tag, Wallet()
-//        transaction.commit()
-//    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+
 
 }
