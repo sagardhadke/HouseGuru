d1a25a3e2dea567e01946663cee2aac4b75e1126
Sagar Dhadke <peaceunique2003@gmail.com>
2023-06-03 13:40:21 +0530

    Confirm Booking Done

---
+++ app/src/main/java/net/uniquecomputer/houseguru/BookingSuccessfully.kt
@@ -0,0 +1,11 @@
+package net.uniquecomputer.houseguru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingSuccessfully : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_successfully)
+    }
+}
